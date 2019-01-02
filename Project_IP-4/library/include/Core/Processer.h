0ac8160004f6413763e0811d9cb57ed9299bac5f
Łukasz Starosta <lstarosta98@gmail.com>
2019-01-02 13:59:15 +0100

    Added truncate to SIDFT

--- Project_IP-4/library/include/Core/Processer.h
+++ Project_IP-4/library/include/Core/Processer.h
@@ -12,9 +12,8 @@ protected:
 	cimg_library::CImg<unsigned char> image;
 	unsigned short int height;
 	unsigned short int width;
-
 	int truncate(double value);
-	
+
 public:
 	Processer();
 	Processer(std::string imageName, int option, double value);
